8d2bd5ec8794522e3644be00cbf3159db35e5d28
Sabine Weber <s1782911@ebirah.inf.ed.ac.uk>
2019-04-14 12:22:23 +0100

    here we go again

--- dbPediaChecker.py
+++ dbPediaChecker.py
@@ -498,32 +498,32 @@ def findStringOverlap():
                 f.write(inStr)
 
 def changeNamespace():
-    """
+    
     f=open("/disk/scratch_big/sweber/GCN-Align/data/de_en/ent_ids_11", "a")
     with open("/disk/scratch_big/sweber/GCN-Align/data/de_en/ent_ids_1","r") as inFile:
         for line in inFile:
             splitLine=line.split("\t") 
             number=int(splitLine[0])
-            newNumber=number-133957
+            newNumber=number-32065
             f.write(str(newNumber)+"\t"+splitLine[1])
-    """
+    
     g=open("/disk/scratch_big/sweber/GCN-Align/data/de_en/triples_11", "a")
     with open("/disk/scratch_big/sweber/GCN-Align/data/de_en/leftovers/triples_1","r") as inFile:
         for line in inFile:
             splitLine=line.split("\t") 
             number=int(splitLine[0])
-            newNumber1=number-133957
-            newNumber2=int(splitLine[2])-133957
+            newNumber1=number-32065
+            newNumber2=int(splitLine[2])-32065
             g.write(str(newNumber1)+"\t"+splitLine[1]+"\t"+str(newNumber2)+"\n")
-    """        
+            
     h=open("/disk/scratch_big/sweber/GCN-Align/data/de_en/ref_ent_ids11","a")
     with open("/disk/scratch_big/sweber/GCN-Align/data/de_en/ref_ent_ids","r") as inFile:
         for line in inFile:
             splitLine=line.split("\t") 
             number=int(splitLine[0])
-            newNumber=number-133957
+            newNumber=number-32065
             h.write(str(newNumber)+"\t"+str(splitLine[1]))
-    """                
+                   
         
     
 
