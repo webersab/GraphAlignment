da6a425006e2de26467264b3e5eb68af1f208841
Sabine Weber <s1782911@ebirah.inf.ed.ac.uk>
2019-04-18 19:52:40 +0100

    trying to print out some graphs

---
+++ showEntGraphs.py
@@ -0,0 +1,66 @@
+import networkx as nx
+import sys
+import matplotlib.pyplot as plt
+import os
+
+if __name__ == "__main__":
+    #filename=sys.argv[1]
+    #filename="/group/project/s1782911/EVENT#LOCATION_sim_HTLFRG"
+    print("hup hup")
+    f=open("/group/project/s1782911/deEntPrintout","a")
+
+    G = nx.Graph()
+    counter=0
+    passedComponent=False
+    passedLambda=False
+    lambdaVal=0
+    
+    for filename in os.listdir("/disk/scratch_big/sweber/entGraph/justGraphs"):
+        with open(filename, 'r') as inF:
+            number=-100
+            for line in inF:
+                if "lambda" in line and not passedLambda:
+                    G = nx.Graph()
+                    lambdaVal=line[8:]
+                    passedLambda=True
+                elif "lambda" in line and passedLambda:
+                    E=nx.connected_components(G)
+                    f=open("/disk/scratch_big/sweber/entGraph/justGraphs/"+filename[45:]+lambdaVal,"a")
+                    for n in E: 
+                        print("-----------------------------------")
+                        f.write("\n-------------------------\n")
+                        for number in n:
+                            print(G.node[number])
+                            f.write(str(G.node[number])+"\n")
+                    G = nx.Graph()
+                    lambdaVal=line[8:]
+                elif "component" in line:
+                    passedComponent=True
+                    lineSplit=line.split()
+                    number=int(lineSplit[1])
+                    G.add_node(number)
+                    #print("added ",number)
+                elif "component" not in line and passedComponent and line!="" and "=>" not in line:
+                    name="verb"+str(counter)
+                    counter+=1
+                    if line!="\n":
+                        G.node[number][name]=line
+                elif "component" not in line and passedComponent and line!="" and "=>" in line: 
+                    lineSplit=line.split()
+                    component=lineSplit[1]
+                    G.add_edge(number, component)
+                elif line=="":
+                    passedComponent=False
+                    
+            E=nx.connected_components(G)
+            for n in E: 
+                print("-----------------------------------")
+                f.write("\n-------------------------\n")
+                for number in n:
+                    print(G.node[number])
+                    f.write(str(G.node[number])+"\n")
+                
+        #nx.draw_networkx_edges(n,pos=nx.spring_layout(n))
+        #plt.show()
+                
+                
