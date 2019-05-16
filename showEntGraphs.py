674f86140738594ac457a29e497d4fb93e0d2132
webersab <sa_bine_weber@yahoo.de>
2019-05-16 11:02:31 +0100

    better graph construction hopefully

--- showEntGraphs.py
+++ showEntGraphs.py
@@ -3,7 +3,42 @@ import sys
 import matplotlib.pyplot as plt
 import os
 
-if __name__ == "__main__":
+def constructGraphFromFile(fileName,lambdaValue):
+    G = nx.Graph()
+    counter=0
+    passedComponent=False
+    passedRightLambda=False
+    
+    with open("/disk/scratch_big/sweber/entGraph/justGraphs/"+filename, 'r') as inF:
+            number=-100
+            for line in inF:
+                if "lambda" in line and str(lambdaValue)==line[8:12]:
+                    G = nx.Graph()
+                    passedComponent=False
+                    passedRightLamda=True
+                    firstLine=line
+                elif "lambda" in line and passedRightLambda:
+                    E=nx.connected_components(G)
+                    return E
+                elif "component" in line and passedRightLambda:
+                    passedComponent=True
+                    lineSplit=line.split()
+                    number=int(lineSplit[1])
+                    G.add_node(number)
+                    #print("added ",number)
+                elif "component" not in line and passedComponent and line!="" and "=>" not in line:
+                    name="verb"+str(counter)
+                    counter+=1
+                    if line!="\n" and number!=-100:
+                        G.node[number][name]=line
+                elif "component" not in line and passedComponent and line!="" and "=>" in line: 
+                    lineSplit=line.split()
+                    component=lineSplit[1]
+                    G.add_edge(number, component)
+                elif line=="":
+                    passedComponent=False
+
+def constructPrintoutsFromFile():
     #filename=sys.argv[1]
     #filename="/group/project/s1782911/EVENT#LOCATION_sim_HTLFRG"
     print("hup hup")
@@ -61,5 +96,10 @@ if __name__ == "__main__":
                 
         #nx.draw_networkx_edges(n,pos=nx.spring_layout(n))
         #plt.show()
+    
+
+if __name__ == "__main__":
+    print("Hup hup graph Construct!")
+    constructGraphFromFile("EVENT#LOCATION_sim_HTLFRG",0.07)
                 
                 
