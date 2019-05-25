be8ae41640ce53b692b6e99b83d1e26da2db0244
webersab <sa_bine_weber@yahoo.de>
2019-05-25 17:42:41 +0100

    added extractEmbeddings now with dict

--- extractEmbeddings.py
+++ extractEmbeddings.py
@@ -3,4 +3,6 @@ import numpy as np
 if __name__ == "__main__":
     data = np.load('/disk/scratch_big/sweber/ent_embeddings.npy')
     print(data.shape)
+    entities_dict = pickle.load( open( "/disk/scratch_big/sweber/entities_dict.p", "rb" ) )
+    print(entities_dict)
     
