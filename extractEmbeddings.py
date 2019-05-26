b31a2d8cd647c49a3af0de0abae3a57f8c0274c9
webersab <sa_bine_weber@yahoo.de>
2019-05-26 15:51:13 +0100

    triny scipy kmeans clustering

--- extractEmbeddings.py
+++ extractEmbeddings.py
@@ -1,5 +1,8 @@
 import numpy as np
 import pickle
+from numpy import vstack,array
+from scipy.cluster.vq import kmeans,vq
+
 
 if __name__ == "__main__":
     ent_embeddings = np.load('/disk/scratch_big/sweber/ent_embeddings.npy')
@@ -7,4 +10,10 @@ if __name__ == "__main__":
     entities_dict = pickle.load( open( "/disk/scratch_big/sweber/entities_dict.p", "rb" ) )
     print("Length : %d" % len(entities_dict))
     #print(entities_dict)
+    
+    centroids,_ = kmeans(ent_embeddings,50000)
+    idx,_ = vq(data,centroids)
+    
+    print(idx)
+    
     
