92324728e2204ab2aaa9c60aae84a1bf151fc01c
webersab <sa_bine_weber@yahoo.de>
2019-05-26 16:10:56 +0100

    try to print vectors as tsv

--- extractEmbeddings.py
+++ extractEmbeddings.py
@@ -2,6 +2,7 @@ import numpy as np
 import pickle
 from numpy import vstack,array
 from scipy.cluster.vq import kmeans,vq
+import collections
 
 
 if __name__ == "__main__":
@@ -9,11 +10,18 @@ if __name__ == "__main__":
     print(ent_embeddings.shape)
     entities_dict = pickle.load( open( "/disk/scratch_big/sweber/entities_dict.p", "rb" ) )
     print("Length : %d" % len(entities_dict))
-    #print(entities_dict)
+    #print(entities_dict)"\t"
     
-    centroids,_ = kmeans(ent_embeddings,50000)
-    idx,_ = vq(data,centroids)
+    np.savetext("ent_embeddings.tsv",ent_embeddings,delimiter="\t")
+    od = collections.OrderedDict(sorted(entities_dict.items()))
+    f=open("entities_dict.tsv", "a")
+    for k, v in od.items():
+        f.write(v+"\n")
     
-    print(idx)
+    
+    #centroids,_ = kmeans(ent_embeddings,50000)
+    #idx,_ = vq(data,centroids)
+    
+    #print(idx)
     
     
