62b52caaacffca313f20f32fe5a8e4e66e52a0ff
Sabine Weber <s1782911@ebirah.inf.ed.ac.uk>
2019-05-27 11:59:18 +0100

    incorporating annoy

--- extractEmbeddings.py
+++ extractEmbeddings.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     
     f = 40
     t = AnnoyIndex(f)  # Length of item vector that will be indexed
-    for i in range(1000):
+    for i in range(100000):
         v = [random.gauss(0, 1) for z in range(f)]
         t.add_item(i, v)
     
