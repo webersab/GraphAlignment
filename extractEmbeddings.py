34f1ac39bd961ba1a065ebb1dfa79fd75c4f5412
Sabine Weber <s1782911@ebirah.inf.ed.ac.uk>
2019-05-27 11:50:58 +0100

    incorporating annoy

--- extractEmbeddings.py
+++ extractEmbeddings.py
@@ -7,24 +7,29 @@ from annoy import AnnoyIndex
 
 
 if __name__ == "__main__":
-    ent_embeddings = np.load('/disk/scratch_big/sweber/rel_embeddings.npy')
-    print(ent_embeddings.shape)
-    entities_dict = pickle.load( open( "/disk/scratch_big/sweber/relations_dict.p", "rb" ) )
-    print("Length : %d" % len(entities_dict))
+    rel_embeddings = np.load('/disk/scratch_big/sweber/rel_embeddings.npy')
+    print(rel_embeddings.shape)
+    relations_dict = pickle.load( open( "/disk/scratch_big/sweber/relations_dict.p", "rb" ) )
+    print("Length : %d" % len(relations_dict))
     #print(entities_dict)"\t"
+    reversed_relations_dict = dict((v,k) for k,v in relations_dict.items())
     
-    """
+    
     f = 200
     t = AnnoyIndex(f)  # Length of item vector that will be indexed
-    for i in xrange(1000):
-        v = [random.gauss(0, 1) for z in xrange(f)]
+    for i in range(419112):
+        v = rel_embeddings[i]
         t.add_item(i, v)
 
-        t.build(10) # 10 trees
+        t.build(100) # 10 trees
         t.save('test.ann')
     
+    u = AnnoyIndex(f)
+    u.load('test.ann') # super fast, will just mmap the file
+    print(u.get_nns_by_item(0, 10))
     
     
+    """
     np.savetxt("ent_embeddings.tsv",ent_embeddings,delimiter="\t")
     f=open("entities_dict.tsv", "a")
     sorted_x = sorted(entities_dict.items(), key=lambda kv: kv[1])
