093d0383f3c8c4661d86d875ed55aaf119faafb4
Sabine Weber <s1782911@ebirah.inf.ed.ac.uk>
2019-05-27 10:50:10 +0100

    incorporating annoy

--- extractEmbeddings.py
+++ extractEmbeddings.py
@@ -3,6 +3,7 @@ import pickle
 from numpy import vstack,array,savetxt
 from scipy.cluster.vq import kmeans,vq
 import collections
+from annoy import AnnoyIndex
 
 
 if __name__ == "__main__":
@@ -12,6 +13,17 @@ if __name__ == "__main__":
     print("Length : %d" % len(entities_dict))
     #print(entities_dict)"\t"
     
+    """
+    f = 200
+    t = AnnoyIndex(f)  # Length of item vector that will be indexed
+    for i in xrange(1000):
+        v = [random.gauss(0, 1) for z in xrange(f)]
+        t.add_item(i, v)
+
+        t.build(10) # 10 trees
+        t.save('test.ann')
+    
+    
     
     np.savetxt("ent_embeddings.tsv",ent_embeddings,delimiter="\t")
     f=open("entities_dict.tsv", "a")
@@ -19,7 +31,7 @@ if __name__ == "__main__":
     sorted_dict = collections.OrderedDict(sorted_x)
     for k, v in sorted_dict.items():
         f.write(k+"\n")
-    
+    """
     
     #centroids,_ = kmeans(ent_embeddings,50000)
     #idx,_ = vq(data,centroids)
