3fa920c20cb60698609a8bf6ec1e52bcd63b0e79
Sabine Weber <s1782911@ebirah.inf.ed.ac.uk>
2019-05-31 11:01:50 +0100

    added detailed error analysis

--- graphErrorAnalysis.py
+++ graphErrorAnalysis.py
@@ -50,14 +50,14 @@ def main(lmbda):
                                 #look for pred1 in graph,
                                 nodesList1, connectedComponent1 = findPredicateInGraph(pred1, G)
                                 if len(nodesList1)>0:
-                                    lineDict[" ".join(nodesList1)]=pred1
+                                    lineDict[" ".join(str(nodesList1))]=pred1
                                 if len(connectedComponent1)>0:
-                                    lineDict[" ".join(connectedComponent1)]="Connected component of "+pred1
+                                    lineDict[" ".join(str(connectedComponent1))]="Connected component of "+pred1
                                 nodesList2, connectedComponent2 = findPredicateInGraph(pred2, G)
                                 if len(nodesList2)>0:
-                                    lineDict[" ".join(nodesList2)]=pred2
+                                    lineDict[" ".join(str(nodesList2))]=pred2
                                 if len(connectedComponent2)>0:
-                                    lineDict[" ".join(connectedComponent2)]="Connected component of "+pred2
+                                    lineDict[" ".join(str(connectedComponent2))]="Connected component of "+pred2
     
             documentDict[lineNumber]=lineDict
     
