7381cd2d2bb87701973f0bc85b2bd3d1af44ca9b
Sabine Weber <s1782911@ebirah.inf.ed.ac.uk>
2019-05-31 14:34:28 +0100

    added detailed error analysis

--- graphErrorAnalysis.py
+++ graphErrorAnalysis.py
@@ -8,6 +8,7 @@ from collections import OrderedDict
 import networkx as nx
 from scipy.special.basic import lmbda
 from tqdm import tqdm
+import pprint
 
 def main(lmbda):
     # constants
@@ -32,9 +33,13 @@ def main(lmbda):
             if line[2]=="y":
                 firstPredicates=xnliTest.extractPredicateFromSentence(model,line[0])
                 secondPredicates=xnliTest.extractPredicateFromSentence(model,line[1])
-                lineDict["predicates"]=list(firstPredicates.keys()).append(list(secondPredicates.keys()))
+                predicatesSet=set()
+                passedPred1=False
+                passedPred2=False
                 for pred1 in firstPredicates.keys():
                     for pred2 in secondPredicates.keys():
+                        predicatesSet.add(pred1)
+                        predicatesSet.add(pred2)
                         typePairList=list(itertools.product(["PERSON","LOCATION","ORGANIZATION","EVENT","MISC"],repeat=2))
                         typePairList.remove(("EVENT","EVENT"))
                         clusterInfoCounter=0
@@ -55,21 +60,24 @@ def main(lmbda):
                                 else:
                                     #look for pred1 in graph,
                                     nodesList1, connectedComponent1 = findPredicateInGraph(pred1, G)
-                                    if len(nodesList1)>0:
+                                    if len(nodesList1)>0 and not passedPred1:
                                         lineDict[" ".join(str(nodesList1))]=pred1
-                                    if len(connectedComponent1)>0:
+                                        passedPred1=True
+                                    if len(connectedComponent1)>0 :
                                         lineDict[" ".join(str(connectedComponent1))]="Connected component of "+pred1
                                     nodesList2, connectedComponent2 = findPredicateInGraph(pred2, G)
-                                    if len(nodesList2)>0:
+                                    if len(nodesList2)>0 and not passedPred2:
                                         lineDict[" ".join(str(nodesList2))]=pred2
+                                        passedPred2=True
                                     if len(connectedComponent2)>0:
                                         lineDict[" ".join(str(connectedComponent2))]="Connected component of "+pred2
-    
+                lineDict["predicates"]=predicatesSet
             documentDict[lineNumber]=lineDict
     
     f=open("errorAnalysis"+str(lmbda)+".txt","a")
-    for k,v in documentDict.items():
-        f.write(str(k)+"\t"+str(v))
+    pprint.pprint(documentDict, f)
+    #for k,v in documentDict.items():
+    #    f.write(str(k)+"\t"+str(v))
 
 def findPredicateInGraph(pred,G):
     nodesList=[]
