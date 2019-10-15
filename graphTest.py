fff11a40ba267b67ee31a8b4c2aaf7456fc83c44
webersab <sa_bine_weber@yahoo.de>
2019-10-15 11:37:33 +0100

    building all the graphs before testing, now for real

--- graphTest.py
+++ graphTest.py
@@ -34,6 +34,7 @@ def generateAllGraphs(lambdaValue):
         graphFile=getRightGraphFile(typePair,lambdaValue)
         graphFileDict[typePair]=graphFile
     
+    print(graphFileDict)
     for typePair in graphFileDict.keys():
         if graphFileDict[typePair]!= "":
             E, G=showEntGraphs.constructGraphFromFile(graphFileDict[typePair], lambdaValue)
